c3fb206601066f895d0bc18f01224813548c8768
mnai01 <imatlak03@gmail.com>
2020-06-06 08:41:34 -0400

    added toast for error handling

--- src/components/Navbar/NavbarUnprotected.js
+++ src/components/Navbar/NavbarUnprotected.js
@@ -8,31 +8,58 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
   Input,
-  Container,
-  Row,
   Col,
+  Button,
+  Form,
 } from 'reactstrap';
+import { GlobalContext } from '../../context/GlobalState';
+import { toast } from 'react-toastify';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Navbar.module.css';
 
 const NavbarUnprotected = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isOpen, setIsOpen] = useState(false);
-  const [transition, setTransition] = useState(false);
+
+  const { login, loginError } = useContext(GlobalContext);
+
+  const Validate = (e) => {
+    console.log(username + ' ' + password);
+
+    if (
+      /^[a-zA-Z0-9_.-]+$/.test(username) &&
+      /^[a-zA-Z0-9_.-]+$/.test(password)
+    ) {
+      console.log(username + ' ' + password);
+      login(username, password);
+
+      // Calling event.persist() on the synthetic event removes the event from the pool allowing references to the event to be retained asynchronously.
+      // e.preventDefault gives error
+      e.persist();
+      console.log(loginError);
+    } else {
+      toast.error('Please enter a Username and Password', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      // Calling event.persist() on the synthetic event removes the event from the pool allowing references to the event to be retained asynchronously.
+      // e.preventDefault gives error
+      e.persist();
+    }
+  };
 
   const toggleNavbar = () => setIsOpen(!isOpen);
 
-  const transitionEnded = () => setTransition(!transition);
-
   return (
-    <div onTransitionEnd={(e) => transitionEnded}>
-      {console.log(transition)}
-      <Navbar sticky='top' color='dark' dark expand='md'>
+    <div>
+      <Navbar sticky='top' color='light' light expand='md'>
         <NavbarBrand tag={Link} to='/'>
           Farfriends
         </NavbarBrand>
@@ -40,11 +67,19 @@ const NavbarUnprotected = () => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className='mr-auto' navbar>
             <Col>
-              <NavItem onClick={toggleNavbar}>
-                <NavLink tag={Link} to='/'>
-                  World
-                </NavLink>
-              </NavItem>
+              {isOpen ? (
+                <NavItem onClick={toggleNavbar}>
+                  <NavLink tag={Link} to='/'>
+                    World
+                  </NavLink>
+                </NavItem>
+              ) : (
+                <NavItem>
+                  <NavLink tag={Link} to='/'>
+                    World
+                  </NavLink>
+                </NavItem>
+              )}
             </Col>
           </Nav>
           {isOpen ? (
@@ -60,10 +95,25 @@ const NavbarUnprotected = () => {
           ) : (
             <>
               <Col xs='12' sm='4' md='3' lg='2' className={classes.navbarInput}>
-                <Input size='sm' type='text' placeholder='Username' />
+                <Input
+                  size='sm'
+                  type='email'
+                  placeholder='Username'
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </Col>
               <Col className={classes.navbarInput} xs='12' sm='4' md='3' lg='2'>
-                <Input size='sm' type='text' placeholder='password' />
+                <Input
+                  size='sm'
+                  type='password'
+                  placeholder='password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Col>
+              <Col xs='0' className={classes.navbarInput}>
+                <Button size='sm' onClick={Validate}>
+                  Login
+                </Button>
               </Col>
             </>
           )}
