586ac6d4912456b4db8e33436dd2191e2649b8da
mnai01 <imatlak03@gmail.com>
2020-06-06 11:15:50 -0400

    id for mock up user data added

--- src/usertestdata.js
+++ src/usertestdata.js
@@ -1,41 +1,46 @@
 import React from 'react';
 
 export const userOptions = [
-  { status: 'online', name: 'Ian Matlak', country: 'United Stated of America' },
+  {
+    status: 'online',
+    name: 'Ian BOK',
+    country: 'United Stated of America',
+    id: 0,
+  },
   {
     status: 'offline',
-    name: 'Ian Matlak',
+    name: 'Sfsd Matlak',
     country: 'United Stated of America',
     id: 1,
   },
   {
     status: 'online',
-    name: 'Ian Matlak',
-    country: 'United Stated of America',
+    name: 'Isdfan Masdftlak',
+    country: 'd Stated of America',
     id: 2,
   },
   {
     status: 'offline',
-    name: 'Ian Matlak',
-    country: 'United Stated of America',
+    name: 'sdfsd dsd',
+    country: 'sdfsd Stated of America',
     id: 3,
   },
   {
     status: 'online',
-    name: 'Ian Matlak',
-    country: 'United Stated of America',
+    name: 'dsfds sdfs',
+    country: 'Unitdsfsded Stsdfated of America',
     id: 4,
   },
   {
     status: 'offline',
-    name: 'Ian Matlak',
-    country: 'United Stated of America',
+    name: 'sa dd',
+    country: 'sa s of America',
     id: 5,
   },
   {
     status: 'online',
-    name: 'Ian Matlak',
-    country: 'United Stated of America',
+    name: 'ggas asg',
+    country: 'ass dsa of aas',
     id: 6,
   },
   {
@@ -48,6 +53,6 @@ export const userOptions = [
     status: 'online',
     name: 'Ian Matlak',
     country: 'United Stated of America',
-    id: 1,
+    id: 10,
   },
 ];
