f521e5b33b038157273236df17424710b235f743
mnai01 <imatlak03@gmail.com>
2020-06-06 00:50:59 -0400

    GlobalProvider implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useMemo } from 'react';
 import './App.css';
+import { GlobalProvider } from './context/GlobalState';
+import Content from './Content';
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Content />
+    </GlobalProvider>
   );
 }
 
