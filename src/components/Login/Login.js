5b6173a6f4b0b975ab00b4d7e2590e210c0dd8e4
mnai01 <imatlak03@gmail.com>
2020-06-06 08:43:04 -0400

    login input validation/toast error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { Route, Link, Redirect } from 'react-router-dom';
 import classes from './Login.module.css';
@@ -8,53 +9,64 @@ export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const { login, auth } = useContext(GlobalContext);
+  const { login, auth, loginError } = useContext(GlobalContext);
 
   const LoginPress = async (e) => {
     console.log(username + ' ' + password);
-    // await login(username, password);
+    await login(username, password);
+    console.log(loginError);
+
+    // if (loginError) {
+    //   toast.error('Could not find account', {
+    //     position: 'top-right',
+    //     autoClose: 3000,
+    //     hideProgressBar: false,
+    //     closeOnClick: true,
+    //     pauseOnHover: true,
+    //     draggable: true,
+    //     progress: undefined,
+    //   });
+    // }
     // Calling event.persist() on the synthetic event removes the event from the pool allowing references to the event to be retained asynchronously.
     // e.preventDefault gives error
     e.persist();
   };
 
-  const RegisterRedirect = (e) => {
-    e.persist();
-    return <Redirect to={{ pathname: '/Register' }} />;
-  };
-
   return (
     <div className={classes.container}>
       {auth && <Redirect to={{ pathname: '/' }} />}
-      <Form className={classes.loginForm}>
+      <AvForm className={classes.loginForm} onValidSubmit={LoginPress}>
         <h1 className='text-center'>Farfriends</h1>
         <FormGroup>
           <Label>Email</Label>
-          <Input
+          <AvField
             type='text'
             name='email'
             placeholder='Email address'
             onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </FormGroup>
         <FormGroup>
           <Label>Password</Label>
-          <Input
+          <AvField
             type='password'
             name='password'
             placeholder='password'
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </FormGroup>
-        <Button size='lg' block onClick={LoginPress}>
+        <Button size='lg' block>
           Login
         </Button>
-        <Button size='lg' block onClick={LoginPress}>
-          <Link className={classes.link} to='/Register'>
+
+        <Link className={classes.link} to='/Register'>
+          <Button className='mt-3' size='lg' block>
             Register
-          </Link>
-        </Button>
-      </Form>
+          </Button>
+        </Link>
+      </AvForm>
     </div>
   );
 }
