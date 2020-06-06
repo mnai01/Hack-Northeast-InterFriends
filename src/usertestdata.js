5a0658c674f62cd443876eec20de6d8b6d3c790e
mnai01 <imatlak03@gmail.com>
2020-06-06 08:41:10 -0400

    test user data

---
+++ src/usertestdata.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export const userOptions = [
+  { status: 'online', name: 'Ian Matlak', country: 'United Stated of America' },
+  {
+    status: 'offline',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 1,
+  },
+  {
+    status: 'online',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 2,
+  },
+  {
+    status: 'offline',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 3,
+  },
+  {
+    status: 'online',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 4,
+  },
+  {
+    status: 'offline',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 5,
+  },
+  {
+    status: 'online',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 6,
+  },
+  {
+    status: 'offline',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 7,
+  },
+  {
+    status: 'online',
+    name: 'Ian Matlak',
+    country: 'United Stated of America',
+    id: 1,
+  },
+];
