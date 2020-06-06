2910c84682f9e97035a77fe612bcc443f4285619
mnai01 <imatlak03@gmail.com>
2020-06-06 08:40:06 -0400

    toast and darkmode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component, useState, useMemo } from 'react';
 import './App.css';
 import { GlobalProvider } from './context/GlobalState';
 import Content from './Content';
+import { ToastContainer, toast } from 'react-toastify';
+
 import {
   Route,
   Link,
@@ -14,6 +16,7 @@ function App() {
   return (
     <GlobalProvider>
       <Content />
+      <ToastContainer />
     </GlobalProvider>
   );
 }
