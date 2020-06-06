5fa2e415d02561d641c4f1954c396115032678d6
mnai01 <imatlak03@gmail.com>
2020-06-06 00:50:48 -0400

    private routes setup

---
+++ src/Content.js
@@ -0,0 +1,42 @@
+import React, { Component, useState, useMemo, useContext } from 'react';
+import './App.css';
+import NavbarProtected from './components/Navbar/NavbarProtected';
+import NavbarUnprotected from './components/Navbar/NavbarUnprotected';
+import Login from './components/Login/Login';
+import { GlobalContext } from './context/GlobalState';
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+const Content = () => {
+  const { auth } = useContext(GlobalContext);
+
+  const PrivateRoute = ({ component: Component, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        render={(props) =>
+          auth ? <Component {...props} /> : <Redirect to={{ pathname: '/' }} />
+        }
+      />
+    );
+  };
+
+  return (
+    <Router>
+      {auth ? <NavbarProtected /> : <NavbarUnprotected />}
+      <Switch>
+        <Route exact path='/Login' component={Login} />
+        <PrivateRoute>
+          <PrivateRoute exact path='/Profile' component={Login} auth={auth} />
+        </PrivateRoute>
+      </Switch>
+    </Router>
+  );
+};
+
+export default Content;
