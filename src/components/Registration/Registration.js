df9815e1fe187f312631e5bb4cfeb03cb71839a9
mnai01 <imatlak03@gmail.com>
2020-06-07 05:09:16 -0400

    Working registration

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -11,6 +11,7 @@ import {
   Col,
 } from 'reactstrap';
 import { Route, Link, Redirect } from 'react-router-dom';
+import { DateInput, DateInputWithLabel } from 'ez-react-date-input';
 import classes from './Registration.module.css';
 import { GlobalContext } from '../../context/GlobalState';
 import { countryOptions } from '../../CountryData';
@@ -27,28 +28,38 @@ export default function Registration() {
   const [nativelanguage, setNativeLanguage] = useState('');
   const [country, setCountry] = useState('');
 
-  const { register } = useContext(GlobalContext);
+  const { register, auth } = useContext(GlobalContext);
 
-  const Register = async (e) => {
+  const Register = (e) => {
     console.log(username + ' ' + password);
-    await register(
+    console.log(
+      'user' + username,
+      'passs' + password,
+      'firstname' + firstname,
+      'lastname' + lastname,
+      DOB
+    );
+    register(
       username,
       password,
       firstname,
       lastname,
-      email,
-      DOB,
-      sex,
-      nativelanguage,
-      country
+      DOB
+      // email,
+      // sex,
+      // nativelanguage,
+      // country
     );
     // Calling event.persist() on the synthetic event removes the event from the pool allowing references to the event to be retained asynchronously.
     // e.preventDefault gives error
     e.persist();
   };
 
+  let today = new Date();
+  let year = today.getFullYear();
   return (
     <div className={classes.container}>
+      {auth && <Redirect to={{ pathname: '/' }} />}
       <AvForm className={classes.loginForm} onValidSubmit={Register}>
         <h1 className='text-center'>Farfriends</h1>
         <Row>
@@ -56,7 +67,7 @@ export default function Registration() {
             <FormGroup>
               <Label>Email</Label>
               <AvField
-                type='text'
+                type='email'
                 name='email'
                 placeholder='First Name'
                 onChange={(e) => setUsername(e.target.value)}
@@ -87,7 +98,7 @@ export default function Registration() {
                 type='text'
                 name='firstname'
                 placeholder='Firstname'
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setFirstname(e.target.value)}
                 required
               />
             </FormGroup>
@@ -99,7 +110,7 @@ export default function Registration() {
                 type='text'
                 name='lastname'
                 placeholder='Lastname'
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setLastname(e.target.value)}
                 required
               />
             </FormGroup>
@@ -112,7 +123,7 @@ export default function Registration() {
               <AvField
                 type='select'
                 name='country'
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setCountry(e.target.value)}
                 name='Choose Country...'
                 required
               >
@@ -124,13 +135,19 @@ export default function Registration() {
         <Row>
           <Col>
             <FormGroup>
-              <Label>DOB</Label>
-              <AvField
-                type='text'
-                name='lastname'
-                placeholder='Lastname'
-                onChange={(e) => setPassword(e.target.value)}
-                required
+              <Label>Date of Birth</Label>
+              <DateInputWithLabel
+                invalidColor='#d65151'
+                className={classes.datePicker}
+                onChange={(e) => {
+                  setDOB(e.value);
+                }}
+                name='myFormName'
+                outputFormat='YYYY-MM-DD'
+                formFormat='DDD-MMM-YYYY'
+                minYear={1900}
+                maxYear={year}
+                valueOnMount={false}
               />
             </FormGroup>
           </Col>
@@ -143,7 +160,7 @@ export default function Registration() {
                 type='select'
                 name='lastname'
                 placeholder='Lastname'
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setSex(e.target.value)}
                 required
               >
                 <option>Male</option>
@@ -160,7 +177,7 @@ export default function Registration() {
                 type='select'
                 name='lastname'
                 placeholder='Lastname'
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setNativeLanguage(e.target.value)}
                 required
               >
                 {languageOptions}
